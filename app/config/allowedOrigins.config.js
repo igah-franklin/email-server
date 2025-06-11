8f80b36c4266ffeedabf3311864630e494c090b3
franklin <igahfranklyn@gmail.com>
2025-06-11 11:02:28 +0100

    email template

--- app/config/allowedOrigins.config.js
+++ app/config/allowedOrigins.config.js
@@ -4,7 +4,8 @@ const allowedOrigins = [
     'http://localhost:3000',
     'https://salonmyculture.online/',
     'https://resumelink.site',
-    'https://resume-camp-igahfranklins-projects.vercel.app'
+    'https://resume-camp-igahfranklins-projects.vercel.app',
+    'https://email-server-beige-six.vercel.app/'
 ];
 
 module.exports = allowedOrigins;
